81210a91e4e0b010adfbabc01b0fc28cc2d298bd
Iago García Suárez <iago.garcia.suarez.ds@gmail.com>
2024-04-04 17:43:45 +0200

    Restored file for reading pavia and displaying dims

--- Makefile
+++ Makefile
@@ -1,20 +1,38 @@
+# CXX=g++
+# CXXFLAGS=-std=c++20 -O2 $(pkg-config --cflags --libs opencv4) -fopenmp -I /usr/include/eigen3
+# LDFLAGS=
+# SOURCES=src/hartigan_omp.cpp
+# OBJECTS=$(SOURCES:.cpp=.o)
+# EXECUTABLE=hartigan_omp
+
+# all: $(SOURCES) $(EXECUTABLE)
+
+# $(EXECUTABLE): $(OBJECTS)
+# 	$(CXX) $(LDFLAGS) $(OBJECTS) -o $@
+
+# .cpp.o:
+# 	$(CXX) $(CXXFLAGS) -c $< -o $@
+
+# clean:
+# 	rm -f $(OBJECTS) $(EXECUTABLE)
+
+# run:
+# 	./$(EXECUTABLE) && rm $(OBJECTS)
+
 CXX=g++
-CXXFLAGS=-std=c++20 -O2 $(pkg-config --cflags --libs opencv4) -fopenmp -I /usr/include/eigen3
-LDFLAGS=
-SOURCES=src/hartigan_omp.cpp
-OBJECTS=$(SOURCES:.cpp=.o)
-EXECUTABLE=hartigan_omp
+CXXFLAGS=-std=c++11 -I /usr/include/eigen3
+SRCDIR=src/
+SRC=hartigan_omp.cpp
+OBJ=$(SRCDIR)$(SRC:.cpp=.o)
+EXEC=hartigan_omp
 
-all: $(SOURCES) $(EXECUTABLE)
+all: $(EXEC)
 
-$(EXECUTABLE): $(OBJECTS)
-	$(CXX) $(LDFLAGS) $(OBJECTS) -o $@
-
-.cpp.o:
-	$(CXX) $(CXXFLAGS) -c $< -o $@
+$(EXEC): $(OBJ)
+	$(CXX) -o $@ $^ $(CXXFLAGS)
 
 clean:
-	rm -f $(OBJECTS) $(EXECUTABLE)
+	rm -f $(OBJ) $(EXEC)
 
-run:
-	./$(EXECUTABLE) && rm $(OBJECTS)
+run: $(EXEC)
+	./$(EXEC)
--- src/hartigan_omp.cpp
+++ src/hartigan_omp.cpp
@@ -4,10 +4,10 @@
 #include <vector>
 #include <random>
 #include <Eigen/Dense>
-#include <opencv2/opencv.hpp>
-#include <omp.h>
+// #include <opencv2/opencv.hpp>
+// #include <omp.h>
 
-const std::string FILE_PATH = "../pavia.txt";
+const std::string FILE_PATH = "./pavia.txt";
 const float HARTIGAN_THRESHOLD = 50.0;
 const int K = 3;
 
@@ -56,73 +56,73 @@ int main() {
         // Imprimir las dimensiones de la matriz
         std::cout << "Dimensiones de la matriz: " << data.rows() << "x" << data.cols() << std::endl;
 
-        double start = omp_get_wtime();
+        // double start = omp_get_wtime();
 
-        // Inicialización de asignaciones y centroides
-        std::mt19937 gen(static_cast<unsigned int>(time(0))); // Semilla con el tiempo actual
-        std::uniform_int_distribution<> dist(0, K);
-        std::vector<int> asignaciones(data.rows() * data.cols());
-        for(auto& val : asignaciones) {
-            val = dist(gen);
-        }
+        // // Inicialización de asignaciones y centroides
+        // std::mt19937 gen(static_cast<unsigned int>(time(0))); // Semilla con el tiempo actual
+        // std::uniform_int_distribution<> dist(0, K);
+        // std::vector<int> asignaciones(data.rows() * data.cols());
+        // for(auto& val : asignaciones) {
+        //     val = dist(gen);
+        // }
 
-        cv::Mat cluster_map(715,1096, CV_8U);
-        cv::Mat matAsignaciones(data.rows(), data.cols(), CV_32S);
-        for(int i = 0; i < data.rows(); ++i) {
-            for(int j = 0; j < data.cols(); ++j) {
-                matAsignaciones.at<int>(i, j) = asignaciones[i * data.cols() + j];
-            }
-        }
-
-        int index = 0;
-        int prev_label = 0;
-
-        for (int i = 0; i < cluster_map.rows; ++i) {
-            for (int j = 0; j < cluster_map.cols; ++j) {  
-                int l = cluster_map.at<uchar>(i, j) = matAsignaciones.at<int>(i,j);
-                if (prev_label != cluster_map.at<unsigned char>(i, j)){
-                    prev_label = cluster_map.at<uchar>(i, j);
-                }
-            }   
-        }
-        
-        std::cout << "Reshaped matrix size: " << cluster_map.size() << std::endl;
-
-        cv::Mat result_image(715,1096, CV_8UC3);
-        cv::Vec3b colors[] = {
-            cv::Vec3b(0, 0, 255),   // Rojo
-            cv::Vec3b(0, 255, 0),   // Verde
-            cv::Vec3b(255, 0, 0),   // Azul
-            cv::Vec3b(255, 255, 0), // Amarillo
-            cv::Vec3b(255, 0, 255), // Magenta
-            cv::Vec3b(0, 255, 255), // Cian
-            cv::Vec3b(255, 255, 255),// Blanco
-            cv::Vec3b(0, 0, 0),     // Negro
-            cv::Vec3b(128, 128, 128),// Gris
-            cv::Vec3b(64, 64, 64)   // Gris oscuro
-        };
-        index = 0;
-        for (int i = 0; i < result_image.rows; ++i) {
-            for (int j = 0; j < result_image.cols; ++j) {
-                uchar cluster_index = cluster_map.at<unsigned char>(index++);
-                result_image.at<cv::Vec3b>(i, j) = colors[cluster_index];
-            }
-        }
-
-        cv::Mat resultT;
-        transpose(result_image,resultT); 
-        cv::imwrite("../results/hartigan_omp_" + std::to_string(K) + ".jpeg",resultT);
-
-        // Mat imagen(height, width, CV_8UC3);
-        // for(int i = 0; i < height; ++i) {
-        //     for(int j = 0; j < width; ++j) {
-        //         int index = i * width + j;
-        //         imagen.at<Vec3b>(i, j) = colors[asignaciones[index]];
+        // cv::Mat cluster_map(715,1096, CV_8U);
+        // cv::Mat matAsignaciones(data.rows(), data.cols(), CV_32S);
+        // for(int i = 0; i < data.rows(); ++i) {
+        //     for(int j = 0; j < data.cols(); ++j) {
+        //         matAsignaciones.at<int>(i, j) = asignaciones[i * data.cols() + j];
         //     }
         // }
-        // cv::imwrite("results/hartigan_omp_" + std::to_string(K) + ".png", imagen);
 
-        // return 0;
+        // int index = 0;
+        // int prev_label = 0;
+
+        // for (int i = 0; i < cluster_map.rows; ++i) {
+        //     for (int j = 0; j < cluster_map.cols; ++j) {  
+        //         int l = cluster_map.at<uchar>(i, j) = matAsignaciones.at<int>(i,j);
+        //         if (prev_label != cluster_map.at<unsigned char>(i, j)){
+        //             prev_label = cluster_map.at<uchar>(i, j);
+        //         }
+        //     }   
+        // }
+        
+        // std::cout << "Reshaped matrix size: " << cluster_map.size() << std::endl;
+
+        // cv::Mat result_image(715,1096, CV_8UC3);
+        // cv::Vec3b colors[] = {
+        //     cv::Vec3b(0, 0, 255),   // Rojo
+        //     cv::Vec3b(0, 255, 0),   // Verde
+        //     cv::Vec3b(255, 0, 0),   // Azul
+        //     cv::Vec3b(255, 255, 0), // Amarillo
+        //     cv::Vec3b(255, 0, 255), // Magenta
+        //     cv::Vec3b(0, 255, 255), // Cian
+        //     cv::Vec3b(255, 255, 255),// Blanco
+        //     cv::Vec3b(0, 0, 0),     // Negro
+        //     cv::Vec3b(128, 128, 128),// Gris
+        //     cv::Vec3b(64, 64, 64)   // Gris oscuro
+        // };
+        // index = 0;
+        // for (int i = 0; i < result_image.rows; ++i) {
+        //     for (int j = 0; j < result_image.cols; ++j) {
+        //         uchar cluster_index = cluster_map.at<unsigned char>(index++);
+        //         result_image.at<cv::Vec3b>(i, j) = colors[cluster_index];
+        //     }
+        // }
+
+        // cv::Mat resultT;
+        // transpose(result_image,resultT); 
+        // cv::imwrite("../results/hartigan_omp_" + std::to_string(K) + ".jpeg",resultT);
+
+        // // Mat imagen(height, width, CV_8UC3);
+        // // for(int i = 0; i < height; ++i) {
+        // //     for(int j = 0; j < width; ++j) {
+        // //         int index = i * width + j;
+        // //         imagen.at<Vec3b>(i, j) = colors[asignaciones[index]];
+        // //     }
+        // // }
+        // // cv::imwrite("results/hartigan_omp_" + std::to_string(K) + ".png", imagen);
+
+        // // return 0;
 
 
     } catch (const std::exception& e) {
